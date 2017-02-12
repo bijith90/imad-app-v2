fbdc5d5028b65893b69dcabe4ffec266ad0e682d
bijith90 <bijith90@gmail.com>
2017-02-12 13:22:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function(){
     counter = counter + 1;
-    res.send(counter);
+    res.send(counter, toString());
 });
 
 app.get('/:articleName', function (req, res){
