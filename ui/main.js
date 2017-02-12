2d70060011b0dcfc31593885b471c14634ad13e2
bijith90 <bijith90@gmail.com>
2017-02-12 16:09:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,17 +3,13 @@ var button = document.getElementById('counter');
 button.onclick = function() {
     // Create a request object
     var request = new XMLHttpRequest();
-    // alert('inside');
+
     request.onreadyStateChange = function() {
-        // alert('inside');
         if(request.readyState === XMLHttpRequest.DONE) {
             console.log('readyState');
             if(request.status === 200) {
-                alert('inside');
                 var counter = request.responseText;
-                alert(counter);
                 var span = document.getElementById('count');
-                alert('sasdasd')
                 span.innerHTML = counter.toString();
             }
         }
